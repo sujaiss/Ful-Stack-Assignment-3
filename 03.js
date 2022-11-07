88b4f83fc3a8fd536c70bdef7566b5710cdd98c1
sujaiss <117624881+sujaiss@users.noreply.github.com>
2022-11-07 11:45:40 +0530

    Create 03.js

---
+++ 03.js
@@ -0,0 +1,6 @@
+cargoHold.splice(3,0,"keys");
+console.log(cargoHold);
+cargoHold.splice(cargoHold.indexOf("instruction manual"),1);
+console.log(cargoHold);
+cargoHold.splice(2,3,"car","fab","string cheese");
+console.log(cargoHold);
