ac3ff6a48c26d6d17ed0a9fb4dac417f3b6a4c4c
sujaiss <117624881+sujaiss@users.noreply.github.com>
2022-11-07 11:47:56 +0530

    Create 04.js

---
+++ 04.js
@@ -0,0 +1,10 @@
+let hs1 = ['duct tape', 'gum', 3.14, false, 6.022e23];
+let hs2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
+let hc=hs1.concat(hs2);
+let t1 = hs1.slice(2,4);
+let t2 =hs2.slice(1,5);
+hs1.reverse();
+//It affects the first array and it Reverse the array
+hs2.sort();
+//It affects the second array and it sort the array
+console.log(hs2)
