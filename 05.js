4a219b556d299fa2029116cab8b382a4f2135725
sujaiss <117624881+sujaiss@users.noreply.github.com>
2022-11-07 11:52:07 +0530

    Create 05.js

---
+++ 05.js
@@ -0,0 +1,18 @@
+let str = 'In space, no one can hear you code.';
+let arr1 = str.split();
+ console.log(arr1)
+let arr2 = str.split('e');
+console.log(arr2)
+let arr3 = str.split(' ')
+console.log(arr3)
+let arr4 = str.split('');
+console.log(arr4)
+let arr = ['B', 'n', 'n', 5]
+console.log(arr.join());
+console.log(arr.join('a'));
+console.log(arr.join(" "));
+console.log(arr.join(''));
+let str2 = "water,space suits,food,plasma sword,batteries"
+let str3 = str2.split(",");
+console.log(str3.sort());
+console.log(str3.join(','));
